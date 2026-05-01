194468362453d54c94a1fc6fc3a63f4b9c6d62a9
Dana Vartanova <dana.vartanova@ht-la.org>
2026-05-01 11:50:10 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner word = new Scanner(System.in);
+        String input = word.nextLine();
+        System.out.println(input);
+    }
+}
